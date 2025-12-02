8db201d0452d94fc758acfe9b8d199390c5628dc
Gaganshetty7 <gaganrshetty07@gmail.com>
2025-12-02 21:00:04 +0530

    made few changes to settings.py to add mysql database

--- job-role-pred-bend/backend/settings.py
+++ job-role-pred-bend/backend/settings.py
@@ -82,8 +82,15 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME':'infy_spring_gagan_db',
+        'USER': 'root',                    # Your MySQL username
+        'PASSWORD': 'gagan@007', # Your MySQL password
+        'HOST': 'localhost',               # Usually localhost
+        'PORT': '3306',
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+        },
     }
 }
 
